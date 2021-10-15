0dd6ccdb7b2852c9984edf89f809c12e5b86d8fa
Aritra-Mondal <aritra.mondal.0538@gmail.com>
2021-10-15 13:46:53 +0530

    frontend completed

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,18 @@
-import React from "react";
-import classes from "./home.module.css";
-
+import Topbar from "../../components/topbar/Topbar";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Feed from "../../components/feed/Feed";
+import Rightbar from "../../components/rightbar/Rightbar";
+import "./home.css"
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed/>
+        <Rightbar/>
+      </div>
+    </>
+  );
 }
